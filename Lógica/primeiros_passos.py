a58f1fea80af350c98a2043821571cf1ce84316a
Felpa7k1 <felipe_ssilva1@outlook.com>
2023-09-19 21:31:57 -0300

    comentários e tipos de variáveis (int,float,string,complex e list)

--- Lógica/primeiros_passos.py
+++
@@ -1,7 +0,0 @@
-#Este sinal serve para comentar um código para que o usuário saiba o que o código faz
-
-"""
-Usando 3 aspas podemos 
-comentar várias linhas
-assim quando o código for executado o script pula essas linhas
-"""
